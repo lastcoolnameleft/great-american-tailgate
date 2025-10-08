e0f8d9bc37ae26e531e3e058ff085ef49aafa430
Tommy Falgout <tommy@lastcoolnameleft.com>
2025-10-07 20:21:27 -0500

    fix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lastcoolnameleft.github.io",
-	base: "/great-american-tailgate",
+	site: "https://www.greatamericantailgate.com",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
